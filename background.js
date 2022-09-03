cd3cd6243d7bb531516a490d96632dfdcbfd4c7d
Ryan Enns <89048358+letsbefriendzz@users.noreply.github.com>
2022-09-03 13:24:25 -0400

    async badness

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
+const RULESET_LIST = [
+    'instagram_post_ruleset',
+    'instagram_comment_ruleset',
+]
 console.log('SocialSnooper Loaded!');
 
-const toggleRequestBlocking = () => {
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if(msg.action === "toggleRulesets") {
+        toggleRulesets();
+    }
+});
 
+function toggleRulesets() {
+    chrome.declarativeNetRequest.getEnabledRulesets(async (ruleSets) => {
+        console.log("Currently enabled rulesets:\n\n", ruleSets);
+        chrome.declarativeNetRequest.updateEnabledRulesets({disableRulesetIds: ruleSets});
+        let rulesToEnable = RULESET_LIST.filter(ruleSet => !ruleSets.includes(ruleSet));
+        if(rulesToEnable.length !== 0)
+            chrome.declarativeNetRequest.updateEnabledRulesets({enableRulesetIds: rulesToEnable});
+    });
 }
--- popup.js
+++ popup.js
@@ -1,34 +1,18 @@
-let rulesets = [
-    'instagram_ruleset',
-    'facebook_ruleset',
-]
 
 updateButtonLabel();
-
-// kinda words kinda doesn't !
 function updateButtonLabel() {
-    const button = document.getElementById("toggleButton");
-    chrome.declarativeNetRequest.getEnabledRulesets((strings) => {
-        // button.value = strings.length;
-        if(strings.length === 0)
-            button.value = "Turn On"
-        else
-            button.value = "Turn Off"
+    const toggle = document.getElementById("toggle");
+    console.log('toggle');
+    chrome.declarativeNetRequest.getEnabledRulesets((enabledRulesets) => {
+        console.log('enabled rulesets:\n\n', enabledRulesets);
+        toggle.checked = enabledRulesets.length !== 0;
     })
 }
 
-function toggleOnBlocking() {
-    chrome.declarativeNetRequest.getEnabledRulesets((strings) => {
-        rulesets.forEach((ruleset, index) => {
-            if(strings.includes(ruleset))
-                chrome.declarativeNetRequest.updateEnabledRulesets({disableRulesetIds: [ruleset]});
-            else
-                chrome.declarativeNetRequest.updateEnabledRulesets({enableRulesetIds: [ruleset]});
-        })
-    })
-    updateButtonLabel();
+function chromeExtTesting() {
+    chrome.runtime.sendMessage({action: "toggleRulesets"});
 }
 
 // inlining the onclick listener isn't allowed in chrome extensions, for whatever reason!
-const toggleButton = document.getElementById("toggleButton");
-toggleButton.addEventListener("click", toggleOnBlocking);
+const toggleButton = document.getElementById("toggle");
+toggleButton.addEventListener("click", chromeExtTesting);
