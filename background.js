911e00e8ac3ce719c465243d69743a4af08efa4b
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-09-09 09:38:56 -0400

    refactor single toggle into multiple

--- background.js
+++ background.js
@@ -1,20 +1,61 @@
-const RULESET_LIST = [
-    "instagram_post_ruleset",
-    "instagram_comment_ruleset",
-    "instagram_follow_ruleset"
-]
+const RULESETS = {
+    instagram_posts_ruleset: "instagram_posts_ruleset",
+    instagram_comments_ruleset: "instagram_comments_ruleset",
+    instagram_follows_ruleset: "instagram_follows_ruleset"
+}
 console.log('SocialSnooper Loaded!');
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    if (msg.action === "toggleRulesets")
-        toggleRulesets();
+    if (msg.action === "togglePostLikes") {
+        console.log('togglePostLikes');
+        togglePostLikes();
+    }
+    if (msg.action === "toggleCommentLikes")
+        toggleCommentLikes();
+    if (msg.action === "toggleFollows")
+        toggleFollows();
 });
 
-function toggleRulesets() {
-    chrome.declarativeNetRequest.getEnabledRulesets(async (ruleSets) => {
-        chrome.declarativeNetRequest.updateEnabledRulesets({disableRulesetIds: ruleSets});
-        let rulesToEnable = RULESET_LIST.filter(ruleSet => !ruleSets.includes(ruleSet));
-        if (rulesToEnable.length !== 0)
-            chrome.declarativeNetRequest.updateEnabledRulesets({enableRulesetIds: rulesToEnable});
+function logEnbaledRulesets() {
+    chrome.declarativeNetRequest.getEnabledRulesets().then((ruleSet) =>{
+        console.log(ruleSet);
+    })
+}
+
+async function togglePostLikes(){
+    await chrome.declarativeNetRequest.getEnabledRulesets().then((ruleSets) => {
+        if (ruleSets.includes(RULESETS.instagram_posts_ruleset)) {
+            return chrome.declarativeNetRequest.updateEnabledRulesets({disableRulesetIds: [RULESETS.instagram_posts_ruleset]});
+        } else {
+            return chrome.declarativeNetRequest.updateEnabledRulesets({enableRulesetIds: [RULESETS.instagram_posts_ruleset]});
+        }
     });
+
+    logEnbaledRulesets();
+}
+
+async function toggleCommentLikes() {
+    await chrome.declarativeNetRequest.getEnabledRulesets().then((ruleSets) => {
+        console.log(ruleSets)
+        if (ruleSets.includes(RULESETS.instagram_comments_ruleset)) {
+            return chrome.declarativeNetRequest.updateEnabledRulesets({disableRulesetIds: [RULESETS.instagram_comments_ruleset]});
+        } else {
+            return chrome.declarativeNetRequest.updateEnabledRulesets({enableRulesetIds: [RULESETS.instagram_comments_ruleset]});
+        }
+    });
+
+    logEnbaledRulesets();
+}
+
+async function toggleFollows() {
+    await chrome.declarativeNetRequest.getEnabledRulesets().then((ruleSets) => {
+        console.log(ruleSets)
+        if (ruleSets.includes(RULESETS.instagram_follows_ruleset)) {
+            return chrome.declarativeNetRequest.updateEnabledRulesets({disableRulesetIds: [RULESETS.instagram_follows_ruleset]});
+        } else {
+            return chrome.declarativeNetRequest.updateEnabledRulesets({enableRulesetIds: [RULESETS.instagram_follows_ruleset]});
+        }
+    });
+
+    logEnbaledRulesets();
 }
--- popup.js
+++ popup.js
@@ -1,15 +1,34 @@
 // inlining the onclick listener isn't allowed in chrome extensions, for whatever reason!
-document.getElementById("toggle").addEventListener("click", sendToggleRulesetsMessage);
+document.getElementById("post-likes").addEventListener("click", sendPostLikesToggleMessage);
+document.getElementById("post-comments").addEventListener("click", sendPostCommentsToggleMessage);
+document.getElementById("follows").addEventListener("click", sendFollowToggleMessage);
 
 // init the button by calling this func once each time the window is opened
-updateTogglePosition();
-function updateTogglePosition() {
-    const toggle = document.getElementById("toggle");
+init();
+
+function init() {
+    updateTogglePosition('follows','instagram_follows_ruleset');
+    updateTogglePosition('post-likes','instagram_posts_ruleset');
+    updateTogglePosition('post-comments','instagram_comments_ruleset');
+}
+
+
+
+function updateTogglePosition(elementId, ruleSetId) {
+    const toggle = document.getElementById(elementId);
     chrome.declarativeNetRequest.getEnabledRulesets((enabledRulesets) => {
-        toggle.checked = enabledRulesets.length !== 0;
+        toggle.checked = enabledRulesets.includes(ruleSetId);
     });
 }
 
-function sendToggleRulesetsMessage() {
-    chrome.runtime.sendMessage({action: "toggleRulesets"});
+function sendPostLikesToggleMessage() {
+    chrome.runtime.sendMessage({action: "togglePostLikes"});
+}
+
+function sendPostCommentsToggleMessage() {
+    chrome.runtime.sendMessage({action: "toggleCommentLikes"});
+}
+
+function sendFollowToggleMessage() {
+    chrome.runtime.sendMessage({action: "toggleFollows"});
 }
